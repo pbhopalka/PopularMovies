98594f68eb07928dfec3196322663e4b4956c643
Piyush Bhopalka <pbhopalka@gmail.com>
2016-06-13 01:10:03 +0530

    Fetch movie data from the API

--- app/src/main/java/com/example/pbhopalka/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/pbhopalka/popularmovies/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.pbhopalka.popularmovies;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -19,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isNetworkConnected())
+            new FetchMovie().execute();
+        else{
+            Toast.makeText(getApplicationContext(), "Please connect to the Internet", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager)  getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = cm.getActiveNetworkInfo();
+        return activeNetInfo != null && activeNetInfo.isConnected();
     }
 
     public class FetchMovie extends AsyncTask<Void, Void, JSONArray> {
@@ -35,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
             final String BASE_URL = "http://api.themoviedb.org/3/discover/movie?";
             final String API_PARAM = "api_key";
 
+            String array;
+
             try{
                 Uri uri = Uri.parse(BASE_URL)
                         .buildUpon()
@@ -45,10 +65,43 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.v(LOG_TAG, "Built URL: " + url.toString());
 
+                connection = (HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                if (stream == null)
+                    return null;
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line;
+
+                while((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0){
+                    return null;
+                }
+
+                array = buffer.toString();
+                Log.v(LOG_TAG, array);
 
             } catch (IOException e){
                 Log.e(LOG_TAG, "Error", e);
                 return null;
+            } finally {
+                if (connection != null)
+                    connection.disconnect();
+                if (reader != null){
+                    try{
+                        reader.close();
+                    } catch (final IOException r){
+                        Log.e(LOG_TAG, "Reader not closed", r);
+                    }
+                }
             }
             return null;
         }
