9e701220b413ddde530fd48a4ccb8dbefc8f12dd
Piyush Bhopalka <pbhopalka@gmail.com>
2016-07-24 17:04:00 +0530

    Add settings to sort movies based on popularity and top_rated

--- app/src/main/java/com/example/pbhopalka/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/pbhopalka/popularmovies/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pbhopalka.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        String EXTRA_SHOW_FRAGMENT = ":android:show_fragment";
+        String EXTRA_NO_HEADERS = ":android:no_headers";
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_settings){
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            settingsIntent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, SettingsActivity.GeneralPreferenceFragment.class.getName());
+            settingsIntent.putExtra(PreferenceActivity.EXTRA_NO_HEADERS, true);
+            startActivity(settingsIntent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
